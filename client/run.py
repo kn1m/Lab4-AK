732c98a49f8757b295a75c5323c262957637e8c0
kn1m <peteknysh@gmail.com>
2015-06-02 01:42:44 +0300

    Cleaned old files.

--- client/run.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("echo 'python runner'")
-os.system("notify-send 'python runner'")
-
-print os.getcwd()
--- server/TCP_server.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by m3sc4 on 16.05.15.
-//
-
-#include "TCP_server.h"
-
-
-TCP_server::TCP_server() { }
